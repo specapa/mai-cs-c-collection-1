ca01e8574f2b20ba383158a98c37a474741c01b5
zeotq <specapa@yandex.ru>
2025-10-09 16:07:31 +0300

    Task 8 empty fix

--- tasks/task_8/main.c
+++ tasks/task_8/main.c
@@ -37,7 +37,7 @@ int main() {
     string = NULL;
 
     long long temp_value;
-    long long max_abs_value = 0;
+    long long max_abs_value = -1;
     unsigned long long i = 0;
 
     while (i < MAX_PROGRAM_ITERATION) {
@@ -60,10 +60,15 @@ int main() {
             continue;
         }
 
-        if (abs(temp_value) > abs(max_abs_value)) max_abs_value = temp_value;
+        if (abs(temp_value) > max_abs_value) max_abs_value = temp_value;
 
         ++i;
         if (i == MAX_PROGRAM_ITERATION) fprintf(stderr, "Main cycle finished at %lld iteration", i);
+    }
+
+    if(max_abs_value == -1) {
+        fprintf(stderr, "Empty input error");
+        return 1;
     }
 
     char *result_string = NULL;
@@ -72,18 +77,24 @@ int main() {
         fprintf(stderr, "At convert_ll_to_n_base: ");
         u_status_print(stderr, status, u_status_to_string);
     }
+
     fprintf(stdout, "%s in %d base <=> %lld in 10 base\n", result_string, base, max_abs_value);
+
     free(result_string);
     result_string = NULL;
+    
     for (unsigned int i = 1; i < 5; ++i) {
         status = convert_ll_to_n_base(max_abs_value, &result_string, i * 9);
         if (status != U_OK) {
             fprintf(stderr, "At convert_ll_to_%d_base: ", i * 9);
             u_status_print(stderr, status, u_status_to_string);
         }
+        
+        fprintf(stdout, "%lld converted to %d base: %s\n", max_abs_value, i * 9, result_string);
+
         free(result_string);
         result_string = NULL;
-        fprintf(stdout, "%lld converted to %d base: %s\n", max_abs_value, i * 9, result_string);
+        
     }
 
     free(result_string);
